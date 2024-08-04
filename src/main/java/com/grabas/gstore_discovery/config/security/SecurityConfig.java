e1c4d5c6f440b18dd2bbd31fba2b3794a15c4138
Gabriel Grabski <gabrielgrabski001@gmail.com>
2024-08-04 17:17:53 -0300

    add secure layer

---
+++ src/main/java/com/grabas/gstore_discovery/config/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.grabas.gstore_discovery.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
